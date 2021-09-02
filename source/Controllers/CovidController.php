7464a27598c395976b42ef2a52544e2f66cfe02b
Caio <bladellano@gmail.com>
2021-09-02 08:54:28 -0300

    Ajustando...

--- source/Controllers/CovidController.php
+++ source/Controllers/CovidController.php
@@ -3,21 +3,3 @@
 namespace Source\Controllers;
 
 use Source\Core\CovidService;
-
-/**
- * Class CovidController
- * @package Source\Controllers
- */
-class CovidController
-{
-
-    /**
-     *
-     */
-    public function casosMensais():void
-    {
-        header('Content-Type: application/json');
-        $resultado = (new CovidService())->totalPorPeriodoJson();
-        die($resultado);
-    }
-}
